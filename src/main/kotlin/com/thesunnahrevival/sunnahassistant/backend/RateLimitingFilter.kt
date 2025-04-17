99c7bc0ce23430d78e07490701d2b4c3b53043d8
Said Muhammad <said.said@zeraki.app>
2025-04-18 02:03:45 +0300

    Revert "`fix(rate-limiting): adjust exclusion logic to bypass non-geocoding-data endpoints`"
    This reverts commit d674f19dcec63d8c89b99d95809eaec3c7e99312.

--- src/main/kotlin/com/thesunnahrevival/sunnahassistant/backend/RateLimitingFilter.kt
+++ src/main/kotlin/com/thesunnahrevival/sunnahassistant/backend/RateLimitingFilter.kt
@@ -57,10 +57,9 @@ class RateLimitingFilter(private val ktorClient: KtorClient) : Filter {
         try {
             val httpRequest = request as HttpServletRequest
             val httpResponse = response as HttpServletResponse
-
-            println("Request received from ${httpRequest.requestURI}")
-            // Exclude non-geocoding-date endpoints from rate limiting and authentication
-            if (!httpRequest.requestURI.contains("geocoding-data")) {
+            
+            // Exclude actuator endpoints from rate limiting and authentication
+            if (httpRequest.requestURI.startsWith("/actuator")) {
                 chain.doFilter(request, response)
                 return
             }
